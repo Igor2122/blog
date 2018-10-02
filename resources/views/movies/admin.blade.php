d54d38d1dc4b8e1789d9cf2be247db0438bac5d9
Igor <igor_pavlenko@yahoo.com>
2018-10-02 14:52:32 +0300

    admin area all movie description hidden to save space, to be added 'edit' functionality

--- resources/views/movies/admin.blade.php
+++ resources/views/movies/admin.blade.php
@@ -3,81 +3,8 @@
 @section('no-container')
 <div class="container-fluid">
    <div class="row">
-     <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-       <div class="sidebar-sticky">
-         <ul class="nav flex-column">
-           <li class="nav-item">
-             <a class="nav-link active" href="#">
-               <span data-feather="home"></span>
-               Dashboard <span class="sr-only">(current)</span>
-             </a>
-           </li>
-           <li class="nav-item">
-           <a class="nav-link" href="{{'/movies/showAllMovies'}}">
-               <span data-feather="film"></span>
-               Movies
-             </a>
-           </li>
-           <li class="nav-item">
-           <a class="nav-link" href="{{ '/movies/addMovie ' }}">
-               <span data-feather="book-open"></span>
-               Add Movie
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="users"></span>
-               Customers
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="bar-chart-2"></span>
-               Reports
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="layers"></span>
-               Integrations
-             </a>
-           </li>
-         </ul>
-
-         <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-           <span>Saved reports</span>
-           <a class="d-flex align-items-center text-muted" href="#">
-             <span data-feather="plus-circle"></span>
-           </a>
-         </h6>
-         <ul class="nav flex-column mb-2">
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="file-text"></span>
-               Current month
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="file-text"></span>
-               Last quarter
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="file-text"></span>
-               Social engagement
-             </a>
-           </li>
-           <li class="nav-item">
-             <a class="nav-link" href="#">
-               <span data-feather="file-text"></span>
-               Year-end sale
-             </a>
-           </li>
-         </ul>
-       </div>
-     </nav>
+     {{-- sidebar for admin navigation --}}
+     @include('partials._admin-nav')
 
      <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
@@ -97,8 +24,30 @@
        {{-- canvas to be added with the grap --}}
        {{-- <canvas class="my-4" id="myChart" width="900" height="380"></canvas> --}}
 
-       
-       @yield('moviesSeciton')
+
+       <div class="row">
+          @foreach ($movieEdit as $movie)
+            <div class="col-md-3">
+              <div class="card mb-4 box-shadow">
+                  <div class="card-title">
+                  </div>
+                  <img src="images/gone_with_wind.jpeg" alt="">                  
+                  <img class="card-img-top" src="{{$movie->image_url}}" alt="Card image cap">
+                  <h1>{{$movie->title}}</h1>
+                  <div class="card-body">
+                  {{-- hidden to save space <p class="card-text">{!! $movie->body !!}</p> --}}
+                    <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                        <a href="/movies/{{$movie->id}}"><button type="button" class="btn btn-sm btn-outline-secondary">View</button></a>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          @endforeach
+      </div>
 
      </main>
    </div>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.master')
 
 @section('content')
-<img src="{{$movie->image_url}}" alt="">
+<div class="container mt-2">
+
+    <h2>{{$movie->title}}</h2>
+    <img src="{{$movie->image_url}}" alt="">
     <h1>{!!$movie->body!!}</h1>
 
+</div>
 @endsection
