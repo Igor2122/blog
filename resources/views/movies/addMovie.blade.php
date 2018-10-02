0ac6e510e200389d10ddc747270d3830f2362e37
Igor <igor_pavlenko@yahoo.com>
2018-10-02 13:18:24 +0300

    addmoveie form updated

--- resources/views/movies/addMovie.blade.php
+++ resources/views/movies/addMovie.blade.php
@@ -5,27 +5,30 @@
 
 @section('moviesSeciton')
 <h2>Add New Movie</h2>
-<form class="w-50 mx-auto mt-5" method="POST" action="/movies">
-  
-
-   {{ csrf_field() }}
-
-      <div class="form-group">
-        <label for="title">Movie Title: </label>
-        <input type="text" class="form-control" id="title" name="title">
-      </div>
-
-      <div class="form-group">
-        <label for="image_url">Image URL:</label>
-        <textarea class="form-control" id="image_url" cols="1" rows="1" name="image_url"></textarea>
-      </div>
-
-      <div class="form-group">
-        <label for="exampleInputPassword1">Description</label>
-        <textarea class="form-control" id="body" cols="30" rows="10" name="body"></textarea>
-      </div>
 
 
-      <button type="submit" class="btn btn-primary">Publish</button>
-</form>
+{!! Form::open(['action' => 'MoviesController@store', 'method' => 'POST', 'class' => 'w-50 mx-auto mt-5']) !!}
+    
+  {{-- <form class="w-50 mx-auto mt-5" method="POST" action="/movies"> --}}
+    {{ csrf_field() }}
+
+        <div class="form-group">
+          <label for="title">Movie Title: </label>
+          <input type="text" class="form-control" id="title" name="title">
+        </div>
+
+        <div class="form-group">
+          <label for="image_url">Image URL:</label>
+          <textarea class="form-control" id="image_url" cols="1" rows="1" name="image_url"></textarea>
+        </div>
+
+        <div class="form-group">
+          <label for="exampleInputPassword1">Description</label>
+          <textarea class="form-control" id="body" cols="30" rows="10" name="body"></textarea>
+        </div>
+
+
+        <button type="submit" class="btn btn-primary">Publish</button>
+  </form>
+{!! Form::close() !!}
 @endsection
